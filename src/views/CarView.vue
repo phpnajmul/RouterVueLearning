e882b29b291af5474d40cebf0196b4f7d26e17d9
phpnajmul <phpnajmul@gmail.com>
2024-02-20 11:54:15 +0600

    I am complete contact view children route

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,10 +1,12 @@
 <script setup>
-  import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
   import cars from "../data/cars.json"
 
   const route = useRoute()
+  const router = useRouter()
+  const carId = parseInt(route.params.id)
 
-  const car = cars.find(c => c.id === parseInt(route.params.id))
+  const car = cars.find(c => c.id === carId)
 
 </script>
 
@@ -12,8 +14,10 @@
 <div>
   <h1>Car View</h1>
   <p>{{car.name}}</p>
-  <p>{{car.price}}</p>
   <p>{{car.year}}</p>
+  <p>{{car.price}}</p>
+  <button @click="router.push(`/cars/${carId}/contact`)">click for contact</button>
+  <RouterView />
 </div>
 </template>
 
---
+++ src/views/ContactView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {useRoute} from "vue-router";
+import cars from "../data/cars.json"
+
+const route = useRoute()
+const car = cars.find(c => c.id === parseInt(route.params.id))
+
+</script>
+
+<template>
+  <div>
+    <h1>
+      Contact Info
+    </h1>
+    {{ car.contact.dealer }}
+    {{ car.contact.number }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
