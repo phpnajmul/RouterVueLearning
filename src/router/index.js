cabe11d89dfab4df39a126beca7cffd4bb93aefd
phpnajmul <phpnajmul@gmail.com>
2024-02-20 11:12:48 +0600

    Complete dynamic product id

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
+import CarView from "@/views/CarView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +18,13 @@ const router = createRouter({
             path: "/about",
             name: "about",
             component: AboutView
+        },
+        {
+            path: "/cars/:id",
+            name: "car",
+            component: CarView
         }
+
     ]
 })
 
---
+++ src/views/CarView.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import {useRoute} from "vue-router";
+  import cars from "../data/cars.json"
+
+  const route = useRoute()
+
+  const car = cars.find(c => c.id === parseInt(route.params.id))
+
+</script>
+
+<template>
+<div>
+  <h1>Car View</h1>
+  <p>{{car.name}}</p>
+  <p>{{car.price}}</p>
+  <p>{{car.year}}</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
